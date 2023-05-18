8a56f2aa1a92969d19fb441123521ec57cc27f7f
George <50774938+gg300@users.noreply.github.com>
2023-05-18 17:09:55 +0300

    Update App.js

--- site/src/App.js
+++ site/src/App.js
@@ -1,5 +1,7 @@
-import logo1 from './robo.svg';
-import home from './home_button.png'
+import logo1 from './proiect-svgs/robo.svg';
+import home from './proiect-svgs/home_button.png';
+import engine from './proiect-svgs/engine.svg';
+import engine2 from './proiect-svgs/engine.svg';
 import './App.css';
 function App() {
   function topFunction() {
@@ -9,9 +11,8 @@ function App() {
 
   return (
     <div className='h-screen bg-fuchsia-900 text-rose-50 from-violet-800 bg-gradient-to-bl'>
-      <div className=' text-rose-50'>
-          <img src={logo1} alt="logo" className='fixed right-4 h-3/6 top-6'/>  
-        </div>
+      
+      <img src={logo1} alt="logo" className='fixed right-4 h-3/6 top-6'/>  
       <div className=' fixed left-3 top-4  text-lg font-mono font-bold '> Inteligenta artificiala
         <div className=' text-center'>viitorul robotilor
           </div> 
@@ -21,16 +22,30 @@ function App() {
       <div className='fixed left-2 text-lg top-48 text-rose-50 border-2 font-serif'>
         <h1 className=' text-3xl h-16'><u>feihgso  {/* add text for left paragraph title */}</u> </h1>
 
-        <p className=' text-center'>hello good lol
+        <p className=' text-center'>hello good ljashfodhs iashfioh siohafioh siuagfoiagsofi saud fisgaiopf oisafgo isgfoisagdfoi o isudfoiu
+        <br>
+        </br>\
+        barwarbattsatabtstbestaiystghtiasits
         {/* ADD text for left paragraph*/}
         </p>
 
         </div>
-      
+      <img src={engine} alt="logo2" className='fixed left-3 bottom-4 h-1/3 '/>
+      <div className='fixed right-3 text-lg bottom-1/4 text-rose-50 border-2 font-serif h-auto w-auto'>
+        <h1 className=' text-3xl h-16'><u>feihgso  {/* add text for left paragraph title */}</u> </h1>
+        <p className=' text-center'>hello good ljashfodhs iashfioh siohafioh siuagfoiagsofi saud fisgaiopf oisafgo isgfoisagdfoi o isudfoiu
+          <br>
+            </br>\
+        barwarbattsatabtstbestaiystghtiasits
+        {/* ADD text for left paragraph*/}
+        </p>
+
+        </div>
       <button onclick={topFunction} id="myBtn" title="Go to top" className=' text-lg text-center absolute bottom-8 right-8'>
         <img src={home} alt="logo" className='rounded-full w-12'/>
         </button>
       </div>
+      
   );
 }
 
